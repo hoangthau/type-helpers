3ba33e90ace6fa05b575d70df6256208c3d74d0c
Thau Nguyen <nghoangthau@gmail.com>
2023-07-06 21:45:19 +0700

    Push types

---
+++ src/02-unions-and-indexing/07-exclude-from-discriminated-union.problem.ts
@@ -0,0 +1,33 @@
+import { Equal, Expect } from "../helpers/type-utils";
+
+export type Event =
+  | {
+      type: "click";
+      event: MouseEvent;
+    }
+  | {
+      type: "focus";
+      event: FocusEvent;
+    }
+  | {
+      type: "keydown";
+      event: KeyboardEvent;
+    };
+
+type NonKeyDownEvents = unknown;
+
+interface A {
+  id: string;
+  name: number
+}
+
+
+type tests = [
+  Expect<
+    Equal<
+      NonKeyDownEvents,
+      | { type: "click"; event: MouseEvent }
+      | { type: "focus"; event: FocusEvent }
+    >
+  >,
+];
